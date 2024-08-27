d8cc14165240aff58b81760c872062c172ba1f93
janisgeegee@gmail.com <janisgeegee@gmail.com>
2024-08-27 12:15:03 +0800

    Created player (triangle) on screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -6,6 +7,14 @@ def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   background_color = (0,0,0)
+  x = SCREEN_WIDTH/2
+  y = SCREEN_HEIGHT/2
+  # Create player
+  player = Player(x,y)
+
+  # Restrict game draw 60FPS
+  clock = pygame.time.Clock()
+  dt = 0
 
   while True:
     # Create close window button
@@ -16,8 +25,17 @@ def main():
     # Fill the screen with black color
     screen.fill(background_color)
 
+    # Draw player
+    player.draw(screen)
+
     # Update the display
     pygame.display.flip()
+
+    # Pause game loop until 1/60 second has pass
+    clock.tick(60)
+    # Time passed how many second
+    dt = clock.get_time()/1000
+    # print(dt)
 
 
 
